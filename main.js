6dae2bd93fcb93434dde54ee7b73ee0d6da63624
damien-simplon <damien.bellec12@gmail.com>
2021-10-01 16:30:46 +0200

    js fonctionnel pour mettre les sections en fonction de la taille de l'écran

--- main.js
+++ main.js
@@ -1,12 +1,9 @@
-function setWindow(){
+window.onload = function(){
     var sections = document.getElementsByClassName("section");
     var taille = window.innerHeight;
     console.log(sections);
     console.log(taille);
     for(let item of sections){
-        console.log("test");
         item.style.height = taille + "px";
     }
 }
-
-setWindow();
