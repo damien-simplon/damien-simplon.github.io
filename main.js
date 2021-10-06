b0dae1b47d4ff9e209151bfe2fe7a454b93d1a61
damien-simplon <damien.bellec12@gmail.com>
2021-10-06 13:00:25 +0200

    suppresion du contenu inutile et commencement du css

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 window.onload = function(){
-    var sections = document.getElementsByClassName("section");
+    var headerCoteGauche = document.getElementById("headerCoteGauche");
+    var headerCoteDroit = document.getElementById("headerCoteDroit");
+    
     var taille = window.innerHeight;
     var tailleLargeur = window.innerWidth;
+
     if(tailleLargeur < 769){
-        for(let item of sections){
-            item.style.height = taille + "px";
-        }
+        headerCoteGauche.style.height = taille + "px";
+        headerCoteDroit.style.height = taille + "px";
     }
 }
