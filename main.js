91602f25507cc56290dd28428f68a72365ada51d
damien-simplon <damien.bellec12@gmail.com>
2021-10-07 16:25:50 +0200

    version mobile quasiment terminée, il reste la partie 'centres d intérêts'

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ window.onload = function(){
     var sections = document.getElementsByClassName("section");
     var headerCoteGauche = document.getElementById("headerCoteGauche");
     var headerCoteDroit = document.getElementById("headerCoteDroit");
+    var interetGrid = document.getElementById("interetGrid");
 
     var taille = window.innerHeight;
     var tailleLargeur = window.innerWidth;
@@ -9,11 +10,14 @@ window.onload = function(){
     if(tailleLargeur < 769){
         for(let item of sections){
             item.style.height = taille + "px";
+            item.style.width = tailleLargeur + "px";
         }
         headerCoteGauche.style.height = taille + "px";
         headerCoteGauche.style.width = tailleLargeur + "px";
 
         headerCoteDroit.style.height = taille + "px";
         headerCoteDroit.style.width = tailleLargeur + "px";
+
+        interetGrid.style.height = tailleLargeur + "px";
     }
 }
