0c54a4e70cbdfddb56746b1f026eacccd8a10512
damien-simplon <damien.bellec12@gmail.com>
2021-10-11 11:46:54 +0200

    finalisation du front pour la version pc et mise en place du bouton pour le darkmode

--- main.js
+++ main.js
@@ -20,4 +20,29 @@ window.onload = function(){
 
         interetGrid.style.height = tailleLargeur + "px";
     }
+    
+    var contenu = document.getElementsByTagName("body");
+    var bouton = document.getElementById("boutonDarkMode");
+    var boutonI = bouton.getElementsByTagName("i");
+    bouton.onclick = function(){
+        for(let item of contenu){
+            if(item.style.backgroundColor == "rgb(79, 78, 78)"){
+                item.style.backgroundColor = "white";
+                item.style.color = "black";
+                bouton.style.border = "1px solid black";
+                for(let item of boutonI){
+                    item.style.color = "black";
+                }
+            }
+            else{
+                item.style.backgroundColor = "rgb(79, 78, 78)";
+                item.style.color = "white";
+                bouton.style.border = "1px solid white";
+                for(let item of boutonI){
+                    item.style.color = "white";
+                }
+            }
+        }
+    }
+
 }
